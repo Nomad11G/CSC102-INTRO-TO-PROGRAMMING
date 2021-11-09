f6ef2fbb53164bb0d712774cc154dba9a75cd938
Nomad11G <90344027+Nomad11G@users.noreply.github.com>
2021-11-09 01:29:11 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -82,3 +82,15 @@ function btrBlastOff(){
         },2000 * i);
     }
 }
+
+function start(){
+    console.log("start() function started");
+    document.getElementById("startButton").disabled = true;
+    document.getElementById("stopButton").disabled = false;
+}
+
+function stop(){
+    console.log("stop() function started");
+    document.getElementById("startButton").disabled = false;
+    document.getElementById("stopButton").disabled = true;
+}
